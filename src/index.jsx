8d39cde5229b3c6cc4c96379f0a22c9beadb6efd
wongalofonso <alfonsowongsd@gmail.com>
2018-08-16 19:29:39 -0700

    working temp page

---
+++ src/index.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ComingSoon from './js/ComingSoon';
+import './css/style.scss';
+
+ReactDOM.render(<ComingSoon/>, document.getElementById('app'));
---
+++ src/js/ComingSoon.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class ComingSoong extends Component {
+  render() {
+    return (
+      <div>
+        <img src = './images/LogoIcons.svg' id = 'main-logo'/>
+      </div>
+    )
+  }
+}

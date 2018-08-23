3207333ddc5c76fe8698f77d06b7fa01d4b136c0
wongalofonso <alfonsowongsd@gmail.com>
2018-08-23 00:23:07 -0700

    added navbar

---
+++ src/js/NavBar.jsx
@@ -0,0 +1,52 @@
+// import React, { Component } from 'react';
+
+
+// export default class NavBar extends Component {
+//   render() {
+//     const { active, scroll } = this.props;
+//     return (
+//       <div>
+//         <ul className='navBar'>
+//           <li>
+//             {
+//               (active === 'splash') ?
+//                 <a className = 'pages'>
+//                   Top
+//                 <img id='topActive' src={Active} onClick = {scroll.bind(this, 'splash-page')}/>
+//                 </a> :
+//                 <a >
+//                   <img id='topMenu' src={Menu} onClick = {scroll.bind(this,'splash-page')}/>
+//                 </a>
+//             }
+//           </li>
+
+//           <li>
+//             {
+//               (active === 'about') ?
+//                 <a className = 'pages'>
+//                   About
+//                   <img id='aboutActive' src={Active} onClick = {scroll.bind(this, 'aboutPage')}/>
+//                 </a> :
+//                 <a >
+//                   <img id='aboutMenu' src={Menu} onClick = {scroll.bind(this, 'aboutPage')}/>
+//                 </a>
+//             }
+//           </li>
+
+//           <li>
+//             {
+//               (active === 'project') ?
+//                 <a className = 'pages'> 
+//                   Projects
+//                   <img id = 'projectActive' src={Active} onClick = {() => scroll.bind(this, 'project-page')}/>
+//                 </a> :
+//                 <a >
+//                   <img src={Menu} onClick = {scroll.bind(this, 'project-page')}/>
+//                 </a>
+//             }
+//           </li>
+//         </ul>
+//       </div>
+//     )
+//   }
+// }
